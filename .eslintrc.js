0712573423b713b29a4012c65e24568e5b72d13e
Kwhong <kwhong74@gmail.com>
2025-12-11 15:40:51 +0900

    📦 모노레포 구조 및 설정 파일 추가
    - pnpm workspace + Turborepo 설정
    - TypeScript, ESLint 설정
    - Vercel 배포 설정
    - .gitignore 설정

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.next/', '.expo/'],
+}
